e68ab270f87288adef93f865dae103c0f08a09e7
illuminationdancing <socoly.s@gmail.com>
2026-05-19 14:47:31 +0300

    Week 1: variables and data types

---
+++ week1/variables.go
@@ -0,0 +1,14 @@
+package main
+import "fmt"
+
+func main() {
+name := "Illumination"
+age := 22
+kiteAngle :=45.5
+isBuilding := true
+
+fmt.Println("Name:", name)
+fmt.Println("Age:", age)
+fmt.Println("Kite angle:", kiteAngle)
+fmt.Println("Building a Career in tech:", isBuilding)
+}
